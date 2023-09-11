f42a48d07dd9730f03ffc1fa733164b9d9474ef3
andresolinoufrn <89939589+andresolinoufrn@users.noreply.github.com>
2023-09-11 23:38:40 +0000

    apenas testes

--- teste.c
+++ teste.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
 int main(){
+  int * x;
+  int y = 10;
 
+  x = &y;
+
+  printf("Ola mundo cruel 2!!!\n");
+  printf("enderedo y = %p\n", &y);
+  printf("ponteiro x = %p\n", x);
+  printf("conteudo x = %d\n", *x);
   return 0;
 
 }
